bb4709d32a664494ef0740cac66dbbb387a18757
gamballo <dlyhbka@gmail.com>
2024-05-08 10:40:02 +0500

    добавлено форма обратной связи, форма карты, не работает топ меню во вкладке КОНТАКТЫ, не работает аунтификация

--- testovaya.php
+++ testovaya.php
@@ -1,15 +1,12 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тестовая");
-?>&nbsp;<?$APPLICATION->IncludeComponent(
-	"bitrix:system.auth.form", 
-	".default", 
-	array(
-		"COMPONENT_TEMPLATE" => ".default",
-		"FORGOT_PASSWORD_URL" => "/user/",
-		"PROFILE_URL" => "/user/profile.php",
-		"REGISTER_URL" => "/user/registration.php",
-		"SHOW_ERRORS" => "N"
+?>&nbsp;<?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth", Array(
+	"COMPONENT_TEMPLATE" => ".default",
+		"FORGOT_PASSWORD_URL" => "/user/",	// Страница забытого пароля
+		"PROFILE_URL" => "/user/profile.php",	// Страница профиля
+		"REGISTER_URL" => "/user/registration.php",	// Страница регистрации
+		"SHOW_ERRORS" => "N",	// Показывать ошибки
 	),
 	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
